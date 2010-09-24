3df2b63d77d88b9c113af888d8d26cc203ea002e
erichhorde <erich@hordeosftware.com>
2010-09-24 09:41:06 -0500

    more google-gbase

--- cookbooks/msm_crons/recipes/default.rb
+++ cookbooks/msm_crons/recipes/default.rb
@@ -1,23 +1,24 @@
 #
 # Cookbook Name:: msm_crons
 # Recipe:: default
-#
-puts node[:instance_role]
-puts node[:instance_role]
-puts node[:instance_role]
-node[:applications].each do |app_name,data|
-  {'gbase-csv-generate'=>[5,0], 'lownotes-csv-generate'=>[5,1]}.each do |job, hour_minute|
-    cron job do
-      action  :create
-      hour hour_minute.first
-      minute hour_minute.last
-      day      '*'
-      month    '*'
-      weekday  '*'
-      command "cd /data/#{app_name}/current && script/runner -e #{node[:environment]} cron/#{job}.rb"
 
-      user node[:owner_name]
-    end  
+if ['solo', 'app', 'app_master'].include?(node[:instance_role])
+  node[:applications].each do |app_name,data|
+    {'gbase-csv-generate'=>[5,0], 
+      'lownotes-csv-generate'=>[5,1], 
+      'google-base-csv-generate'=>[5,2]}.each do |job, hour_minute|
+      cron job do
+        action  :create
+        hour hour_minute.first
+        minute hour_minute.last
+        day      '*'
+        month    '*'
+        weekday  '*'
+        command "cd /data/#{app_name}/current && script/runner -e #{node[:environment]} cron/#{job}.rb"
+
+        user node[:owner_name]
+      end  
     
+    end
   end
 end
