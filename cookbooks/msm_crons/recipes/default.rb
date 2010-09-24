72393a22ec7b527d92254bc7ef1fa272195e7dd4
erichhorde <erich@hordeosftware.com>
2010-09-24 10:05:08 -0500

    oops

--- cookbooks/msm_crons/recipes/default.rb
+++ cookbooks/msm_crons/recipes/default.rb
@@ -6,7 +6,9 @@ if ['solo', 'app', 'app_master'].include?(node[:instance_role])
   node[:applications].each do |app_name,data|
     {'gbase-csv-generate'=>[5,0], 
       'lownotes-csv-generate'=>[5,1], 
-      'google-base-csv-generate'=>[5,2]}.each do |job, hour_minute|
+      'google-base-csv-generate'=>[5,2],
+      'oodle-csv-generate'=>[5,3],
+      'mygear-csv-generate'=>[5,4]}.each do |job, hour_minute|
       cron job do
         action  :create
         hour hour_minute.first
@@ -18,7 +20,15 @@ if ['solo', 'app', 'app_master'].include?(node[:instance_role])
 
         user node[:owner_name]
       end  
-    
     end
+    cron 'mygear-csv-generate' do
+      action  :create
+      hour '5'
+      minute '10'
+      day      '*'
+      month    '*'
+      weekday  '*'
+      command "cd /data/#{app_name}/current && ruby cron/mygear-csv-generate.rb production"
+    end  
   end
 end
