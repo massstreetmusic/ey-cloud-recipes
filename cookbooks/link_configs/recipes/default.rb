e7547c0db743988308aa7cbd51a257d72d5deb46
erichhorde <erich@hordeosftware.com>
2010-11-19 13:37:04 -0600

    reworking sym linking

--- cookbooks/link_configs/recipes/default.rb
+++ cookbooks/link_configs/recipes/default.rb
@@ -4,7 +4,10 @@
 #
 
 node[:applications].each do |app_name,data|
-  link "/data/#{app_name}/current/config/amazon_s3.yml" do
-    to "/data/#{app_name}/shared/config/amazon_s3.yml"
-  end
+  # link "/data/#{app_name}/current/config/amazon_s3.yml" do
+  #   to "/data/#{app_name}/shared/config/amazon_s3.yml"
+  # end
+  run "ln -nfs #{shared_path}/config/memcached_custom.yml #{release_path}/config/memcached.yml"
+  run "ln -nfs #{shared_path}/config/amazon_s3.yml #{release_path}/config/amazon_s3.yml"
+  
 end
