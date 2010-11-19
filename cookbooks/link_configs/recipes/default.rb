b3e9a099efc0e49205d78dd844a6c615ce800878
erichhorde <erich@hordeosftware.com>
2010-11-19 14:08:19 -0600

    more wrestingl with chef

--- cookbooks/link_configs/recipes/default.rb
+++ cookbooks/link_configs/recipes/default.rb
@@ -7,7 +7,7 @@ node[:applications].each do |app_name,data|
   # link "/data/#{app_name}/current/config/amazon_s3.yml" do
   #   to "/data/#{app_name}/shared/config/amazon_s3.yml"
   # end
-  run "ln -nfs /data/#{app_name}/shared/config/memcached_custom.yml /data/#{app_name}/current/config/memcached.yml"
-  run "ln -nfs /data/#{app_name}/shared/config/config/amazon_s3.yml /data/#{app_name}/current/config/amazon_s3.yml"
+  command "ln -nfs /data/#{app_name}/shared/config/memcached_custom.yml /data/#{app_name}/current/config/memcached.yml"
+  command "ln -nfs /data/#{app_name}/shared/config/config/amazon_s3.yml /data/#{app_name}/current/config/amazon_s3.yml"
   
 end
