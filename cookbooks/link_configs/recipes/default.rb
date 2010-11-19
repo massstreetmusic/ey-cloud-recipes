239d53db3ba351b86b8b9aa203510efa6c521f5a
erichhorde <erich@hordeosftware.com>
2010-11-19 14:03:23 -0600

    correct path reference

--- cookbooks/link_configs/recipes/default.rb
+++ cookbooks/link_configs/recipes/default.rb
@@ -7,7 +7,7 @@ node[:applications].each do |app_name,data|
   # link "/data/#{app_name}/current/config/amazon_s3.yml" do
   #   to "/data/#{app_name}/shared/config/amazon_s3.yml"
   # end
-  run "ln -nfs #{shared_path}/config/memcached_custom.yml #{release_path}/config/memcached.yml"
-  run "ln -nfs #{shared_path}/config/amazon_s3.yml #{release_path}/config/amazon_s3.yml"
+  run "ln -nfs /data/#{app_name}/shared/config/memcached_custom.yml /data/#{app_name}/current/config/memcached.yml"
+  run "ln -nfs /data/#{app_name}/shared/config/config/amazon_s3.yml /data/#{app_name}/current/config/amazon_s3.yml"
   
 end
