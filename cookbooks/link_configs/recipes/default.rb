38750568024e0a596d2d14a0167e847570ac6610
erichhorde <erich@hordeosftware.com>
2010-11-19 14:10:11 -0600

    argh!

--- cookbooks/link_configs/recipes/default.rb
+++ cookbooks/link_configs/recipes/default.rb
@@ -7,7 +7,7 @@ node[:applications].each do |app_name,data|
   # link "/data/#{app_name}/current/config/amazon_s3.yml" do
   #   to "/data/#{app_name}/shared/config/amazon_s3.yml"
   # end
-  command "ln -nfs /data/#{app_name}/shared/config/memcached_custom.yml /data/#{app_name}/current/config/memcached.yml"
-  command "ln -nfs /data/#{app_name}/shared/config/config/amazon_s3.yml /data/#{app_name}/current/config/amazon_s3.yml"
+  execute "ln -nfs /data/#{app_name}/shared/config/memcached_custom.yml /data/#{app_name}/current/config/memcached.yml"
+  execute "ln -nfs /data/#{app_name}/shared/config/config/amazon_s3.yml /data/#{app_name}/current/config/amazon_s3.yml"
   
 end
